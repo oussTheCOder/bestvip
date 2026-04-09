b45dd1a40a61d4437abbc22a9f6c33432cc7f916
oussTheCOder <ouss.coder92@gmail.com>
2026-04-09 10:30:05 +0100

    Add Google Search Console verification meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { LanguageProvider } from '../contexts/LanguageContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <LanguageProvider>
+      <Head>
+        <meta name="google-site-verification" content="BuOzxUEd2kdWZCruhT09JuNZkWqyd7is8gsO4IJlv-0" />
+      </Head>
       <Component {...pageProps} />
     </LanguageProvider>
   );
